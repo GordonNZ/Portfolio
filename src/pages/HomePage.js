5e4400a25c28f23bfbc952bc768fa84671a90b50
Gordon Zam <gordonzam@gmail.com>
2023-04-27 19:32:38 +1200

    added mouse blob, scroll animations, layout edits

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,13 +6,6 @@ import Footer from './components/Footer';
 import '../App.css';
 
 export default function Home() {
-  // const blob = document.querySelector('.blob');
-  // document.body.onpointermove = (e) => {
-  //   const { clientX, clientY } = e;
-
-  //   blob.style.left = `${clientX}px`;
-  //   blob.style.top = `${clientY}px`;
-  // };
   return (
     <div className='homePage'>
       <NavBar />
--- src/pages/components/Main.js
+++ src/pages/components/Main.js
@@ -19,21 +19,39 @@ import { MissionXSlides } from './MissionXSlides';
 export default function Main() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const blobRef = useRef(null);
+  const section = useRef([]);
+
+  useEffect(() => {
+    // Create a new Intersection Observer
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // If the element is intersecting, add the 'show' class
+          entry.target.classList.add('show');
+        }
+      });
+    });
+    // Observe each section element using the observer
+    section.current.forEach((el) => {
+      observer.observe(el);
+    });
+    //disconnect the observer when the component is unmounted to avoid memory leaks.
+    return () => observer.disconnect();
+  }, []);
 
   useEffect(() => {
     if (blobRef.current) {
-      if (mousePosition.y + position < pageHeight - 100) {
-        blobRef.current.animate(
-          {
-            left: `${mousePosition.x}px`,
-            top: `${mousePosition.y + position}px`,
-          },
-          {
-            duration: 500,
-            fill: 'forwards',
-          }
-        );
-      }
+      // if (mousePosition.y + position) {
+      blobRef.current.animate(
+        {
+          left: `${mousePosition.x}px`,
+          top: `${mousePosition.y + position}px`,
+        },
+        {
+          duration: 1500,
+          fill: 'forwards',
+        }
+      );
     }
   }, [mousePosition]);
 
@@ -49,45 +67,39 @@ export default function Main() {
 
   function handleMouseMove(e) {
     setMousePosition({ x: e.clientX, y: e.clientY });
+
+    const newX = e.clientX;
+    const newY = e.clientY;
+
+    // Check if the div is at the bottom of the page
+    const windowHeight = window.innerHeight;
+    const divHeight = blobRef.current.offsetHeight;
+    if (newY + divHeight > windowHeight + 100) {
+      setMousePosition({ x: newX, y: windowHeight - divHeight + 100 });
+    }
   }
 
   useEffect(() => {
-    // function handleMouseMove(e) {
-    //   setPosition({ x: e.clientX, y: e.clientY });
-    // }
     document.addEventListener('mousemove', handleMouseMove);
     return () => {
       document.removeEventListener('mousemove', handleMouseMove);
     };
   }, []);
 
-  const [pageHeight, setPageHeight] = useState(0);
-
-  useEffect(() => {
-    function updatePageHeight() {
-      setPageHeight(document.documentElement.scrollHeight);
-    }
-
-    updatePageHeight();
-    window.addEventListener('resize', updatePageHeight);
-    return () => {
-      window.removeEventListener('resize', updatePageHeight);
-    };
-  }, []);
-
   return (
     <main>
       <div
-        style={{ left: mousePosition.x, top: mousePosition.y }}
+        style={{
+          left: mousePosition.x,
+          top: mousePosition.y,
+        }}
         className='blob'
         ref={blobRef}
       ></div>
-      <div className='blur'></div>
-      <section>
-        <div className='mainHeaders'>
-          <div className='mainH2Line'></div>
-          <h2 className='mainH2 firstH2'>Background</h2>
-        </div>
+      {/* <div className='blur' ref={divRef}></div> */}
+      <section className='hidden' ref={(el) => section.current.push(el)}>
+        <h2 className='mainH2 firstH2'>Background</h2>
+
         <div className='articleFlex'>
           <article>
             <h3 className='mainH3'>Mission Ready</h3>
@@ -103,14 +115,14 @@ export default function Main() {
           <article>
             <h3 className='mainH3'>Auckland University of Technology</h3>
             <p>
-              I recently graduated with a Bachelors of Creative Technologies
-              from AUT. This is what pushed me to pursue web development as a
-              career through Mission Ready.
+              I recently graduated with a Bachelor of Creative Technologies from
+              AUT. This is what pushed me to pursue web development as a career
+              through Mission Ready.
             </p>
           </article>
         </div>
       </section>
-      <section>
+      <section className='hidden' ref={(el) => section.current.push(el)}>
         <h2 className='mainH2'>Tools</h2>
         <div className='mainGrid'>
           <div className='mainGridItem'>
@@ -147,7 +159,10 @@ export default function Main() {
           </div>
         </div>
       </section>
-      <section className='mainProjectSection'>
+      <section
+        className='mainProjectSection hidden'
+        ref={(el) => section.current.push(el)}
+      >
         <h2 className='mainH2'>Projects</h2>
         <article className='mainProjectArticle'>
           <div className='projectSlideContainer'>
@@ -164,10 +179,10 @@ export default function Main() {
             </a>
           </div>
           <p className='projectDesc'>
-            A todo app I built using react hooks with React.js. Key
-            functionalities include the ability to add task title, description,
-            completion date, colour selection, as well as the capability to mark
-            tasks as complete or remove them entirely.
+            A todo app I built with React.js. <br />
+            Key functionalities include the ability to add task title,
+            description, completion date, colour selection, as well as the
+            capability to mark tasks as complete or remove them entirely.
           </p>
         </article>
         <article className='mainProjectArticle'>
@@ -193,8 +208,8 @@ export default function Main() {
             Express.js. The database was constructed utilizing MySQL Workbench.
             <br />
             This also included regular communication with the team; reviewing
-            our progress, what we needed to do, helping one another and regular
-            code merging.
+            our progress, what we needed to complete, helping one another and
+            regular code merging.
           </p>
         </article>
       </section>
